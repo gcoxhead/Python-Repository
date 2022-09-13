11e20d1d07fd3f3ad178db203c0fb0255279875f
Gary Coxhead <79542013+gcoxhead@users.noreply.github.com>
2022-09-13 15:20:03 +0100

    Add files via upload

---
+++ stringToUpper.py
@@ -0,0 +1,10 @@
+# Converts a string to all upper case
+
+word = "upper"
+
+print("Original word is " + word)
+
+word = word.upper()
+
+print("Uppercase word is " + word)
+# Write your code here :-)
