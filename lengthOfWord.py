97dc486ffd5b4ad92d887237536660b0fb65c9c8
Gary Coxhead <79542013+gcoxhead@users.noreply.github.com>
2022-09-13 15:29:38 +0100

    Add files via upload

---
+++ lengthOfWord.py
@@ -0,0 +1,7 @@
+text = input("Type a word to find out how many characters are in it... ")
+
+lengthText = str(len(text))
+
+print("There are " + lengthText +" characters in the string " + text)
+
+# Write your code here :-)
