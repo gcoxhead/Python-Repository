71a494ccca019871c2491581422447fcb68cf9be
Gary Coxhead <79542013+gcoxhead@users.noreply.github.com>
2026-02-01 16:19:54 +0000

    Uodate
    Added time elapsed

--- maker_pi_playground.py
+++ maker_pi_playground.py
@@ -5,19 +5,35 @@ import dht
 # sensor = dht.DHT22(Pin(27))
 sensor = dht.DHT11(Pin(27))
 
-def print_sensor_readings():
+reading_number = 0
+seconds_elapsed = 0
+
+def print_sensor_readings(reading_number, seconds_elapsed):
     temp = sensor.temperature()
     hum = sensor.humidity()
+    
+    print("Time elapsed: " + format_time(seconds_elapsed))
+    
+    print(f"Reading number:  {reading_number}")
     print('Temperature: %3.1f C' %temp)
 #   print('Temperature: %3.1f F' %temp_f)
     print('Humidity: %3.1f %%\n' %hum)
 #   temp_f = temp * (9/5) + 32.0
+    
+def format_time(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"   
 
 while True:
   try:
     sleep(2)
+    seconds_elapsed+=2
     sensor.measure()
-    print_sensor_readings()
+    reading_number+=1
+    
+    print_sensor_readings(reading_number, seconds_elapsed)
     
   except OSError as e:
     print('Failed to read sensor.')
