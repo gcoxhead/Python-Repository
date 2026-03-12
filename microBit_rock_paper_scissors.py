405fae19bea5b10dc9692b7b50ea2804295a81de
Gary Coxhead <79542013+gcoxhead@users.noreply.github.com>
2026-03-12 12:51:56 +0000

    Create microBit_rock_paper_scissors.py

---
+++ microBit_rock_paper_scissors.py
@@ -0,0 +1,32 @@
+#microbit rock paper scissors game
+
+hand = 0
+
+def on_gesture_shake():
+    global hand
+    hand = randint(1, 3)
+    if hand == 1:
+        basic.show_leds("""
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            """)
+    elif hand == 2:
+        basic.show_leds("""
+            # . . . .
+            # # . . .
+            . # # . .
+            . . # # .
+            . . . # #
+            """)
+    else:
+        basic.show_leds("""
+            . . . . .
+            # # . . #
+            # . # # .
+            # # . . #
+            . . . . .
+            """)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
